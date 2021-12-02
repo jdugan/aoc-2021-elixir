5d3f2d018423d8c9661127eebfa4338b32adfc29
John Dugan <34268+jdugan@users.noreply.github.com>
2021-12-02 19:40:23 +0100

    Day 2

--- lib/day01/day01.ex
+++ lib/day01/day01.ex
@@ -1,6 +1,24 @@
 defmodule Day01 do
+  @moduledoc """
+  This day's puzzle had a fun little trick. In part two, the
+  windows being compared mostly contain the same values—the
+  last two values of Window A are the same as the first two
+  values of Window B.
 
-  #========== PUBLIC FNS ==================================
+  So, to compare the sums of the windows, we really only need
+  to compare the values which are distinct—i.e., the first value
+  of Window A and the last value of Window B.
+
+  This means part 1 and part 2 use the same solution
+  pattern. The only difference is how far apart the numbers
+  being compared are in the list.  In part 1, the numbers
+  are 1 position apart (i.e., adjacent); in part 2, they
+  are 3 positions apart.
+  """
+
+  # -------------------------------------------------------
+  # Public Methods
+  # -------------------------------------------------------
 
   def both do
     IO.puts(" ")
@@ -11,22 +29,19 @@ defmodule Day01 do
   end
 
   def puzzle1 do
-    countIncreasesByWindow(1)
+    countIncreasesByWindowSize(1)
   end
 
   def puzzle2 do
-    countIncreasesByWindow(3)
+    countIncreasesByWindowSize(3)
   end
 
 
-  #========== PRIVATE FNS =================================
+  # -------------------------------------------------------
+  # Private Methods
+  # -------------------------------------------------------
 
-  defp data do
-    Reader.to_lines("./data/day01/input.txt") |>
-      Enum.map(&String.to_integer/1)
-  end
-
-  defp countIncreasesByWindow (window) do
+  defp countIncreasesByWindowSize(window) do
     countIncreases(data(), 0, window, 0)
   end
 
@@ -40,6 +55,14 @@ defmodule Day01 do
     else
       countIncreases(list, i0 + 1, i1 + 1, count)
     end
+  end
+
+
+  # ========== DATA HELPERS ===============================
+
+  defp data do
+    Reader.to_lines("./data/day01/input.txt")
+    |> Enum.map(&String.to_integer/1)
   end
 
 end
