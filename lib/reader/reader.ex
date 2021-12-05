945881c2cc2f75c3aa09e81f5b7ce7d29f1d34f8
John Dugan <34268+jdugan@users.noreply.github.com>
2021-12-05 08:34:14 +0100

    Day 5

--- lib/reader/reader.ex
+++ lib/reader/reader.ex
@@ -1,8 +1,12 @@
 defmodule Reader do
-  # ========== PUBLIC FNS ==================================
+
+  # -------------------------------------------------------
+  # Public Methods
+  # -------------------------------------------------------
 
   def to_lines(path) do
     {_, raw} = File.read(path)
     raw |> String.split("\n", trim: true)
   end
+
 end
